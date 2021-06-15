f65ea2274be78c34ca0198f8575e439b324e32a3
han-lh <hoanghan.le.95@gmail.com>
2021-06-15 14:07:34 +0700

    Getting actual weather data from the openweathermap API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:bootcamp_clima/services/location.dart';
+import 'package:bootcamp_clima/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+const apiKey = 'e1c494c8f27d2bc4ba2cc821ee1d4bca';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,42 +10,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude = 0;
+  double longitude = 0;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = new Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+      url: Uri.https('samples.openweathermap.org', '/data/2.5/weather', {
+        'lat': latitude,
+        'lon': longitude,
+        'appid': apiKey,
+      }),
+    );
+
+    var weatherData = await networkHelper.getData();
   }
 
-  void getData() async {
-    var url = Uri.https('samples.openweathermap.org', '/data/2.5/weather', {
-      'lat': '35',
-      'lon': '139',
-      'appid': 'e1c494c8f27d2bc4ba2cc821ee1d4bca'
-    });
-    http.Response response = await http.get(url);
-    if (response.statusCode == 200) {
-      String data = response.body;
-      var decodeData = jsonDecode(data);
-
-      double temperature = decodeData['main']['temp'];
-      int condition = decodeData['weather'][0]['id'];
-      String cityName = decodeData['name'];
-
-      print(temperature);
-      print(condition);
-      print(cityName);
-    } else {
-      print(response.statusCode);
-    }
-  }
+  void getData() async {}
 
   @override
   Widget build(BuildContext context) {
